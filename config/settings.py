aaebc02b8bf385956d9a13d2a4c119d90cbbaa55
Stanislav <your_email@example.com>
2025-05-15 10:56:15 +0500

    wip

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'sync' / 'db' / 'db.sqlite3',
     }
 }
 
